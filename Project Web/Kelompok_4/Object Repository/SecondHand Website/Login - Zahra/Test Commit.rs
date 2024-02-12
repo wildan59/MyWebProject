0507c3a2035a660fa5282585b3d37cf507cbf646
zarabiba <fzahrahabibah@gmail.com>
2024-02-12 21:38:42 +0700

    Add Object Rep ID01_Zahra

---
+++ Project Web/Kelompok_4/Object Repository/SecondHand Website/Login - Zahra/Test Commit.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Test Commit</name>
+   <tag></tag>
+   <elementGuidId>f530a596-ef3a-410c-a07d-23924cc282df</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
