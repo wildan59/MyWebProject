575623893723cbe72f8098eefdb6306f84068b1b
wildanbinar <asep.wildan02@gmail.com>
2024-02-19 19:28:04 +0700

    Inisiasi Project App

---
+++ Project Web/Kelompok_4/Object Repository/SecondHand Website/04 Product/Produk_Tambah Produk.rs
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Produk_Tambah Produk</name>
+   <tag></tag>
+   <elementGuidId>20972f9a-c904-4e8c-ac93-7bdbff62093e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>a.new-product-button.h-100.w-100.border-2.rounded-4.text-black-50.d-flex.align-items-center.justify-content-center</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Terjual'])[1]/following::a[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>7aab5cde-9495-4cfd-98ed-88b9fca40e71</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>new-product-button h-100 w-100 border-2 rounded-4 text-black-50 d-flex align-items-center justify-content-center</value>
+      <webElementGuid>75953440-c004-41cb-b064-c64aa871f544</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/products/new</value>
+      <webElementGuid>75598be2-16b6-41a1-a4fc-d95c5641dba1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+              
+                
+                Tambah Produk
+              
+</value>
+      <webElementGuid>11d2089f-f668-47e3-87b1-9860b3d2406c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/section[@class=&quot;pt-5 mt-5&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;row my-5&quot;]/div[@class=&quot;col-12 col-lg-9 mt-5 mt-lg-0&quot;]/div[@class=&quot;row g-4&quot;]/div[@class=&quot;col-12 col-lg-4&quot;]/a[@class=&quot;new-product-button h-100 w-100 border-2 rounded-4 text-black-50 d-flex align-items-center justify-content-center&quot;]</value>
+      <webElementGuid>954cd63c-3a00-4ac5-a083-f4fae60f91e0</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Terjual'])[1]/following::a[1]</value>
+      <webElementGuid>92a3011d-2f86-4254-b81f-f87585a98ffa</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Diminati'])[1]/following::a[2]</value>
+      <webElementGuid>15e03592-bfdf-4970-82b3-1d5aa8101d74</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Piring cantik'])[1]/preceding::a[1]</value>
+      <webElementGuid>cfc884e4-c89a-4471-bb11-f70ad013928d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/products/new')]</value>
+      <webElementGuid>d070a47c-5b63-4cce-9b20-8bceab0b5732</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/a</value>
+      <webElementGuid>ee8adfd7-323e-4f0b-a606-ebf497326b20</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '/products/new' and (text() = '
+              
+                
+                Tambah Produk
+              
+' or . = '
+              
+                
+                Tambah Produk
+              
+')]</value>
+      <webElementGuid>37ffb610-0fa7-49f8-a483-6fddac6a212a</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Project Web/Kelompok_4/Object Repository/SecondHand Website/08 Produk Terjual/Page_SecondHand/Produk_Terjual_Title.rs
@@ -0,0 +1,119 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Produk_Terjual_Title</name>
+   <tag></tag>
+   <elementGuidId>be6f8f4c-69da-45d5-8020-c5b5856b7c73</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.col-12.col-lg-4</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Terjual'])[1]/following::div[3]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>e14e1b76-d584-4f3b-98ac-26a9e1c84eea</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>col-12 col-lg-4</value>
+      <webElementGuid>1ea09c3d-d112-4549-a760-123c5c4e8ee7</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+              
+  
+      
+
+    
+      Piring cantik
+      Hobi
+      Rp 22.000
+    
+  
+
+            </value>
+      <webElementGuid>2787a1fe-fb13-4121-9764-d31f748e5ffa</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/section[@class=&quot;pt-5 mt-5&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;row my-5&quot;]/div[@class=&quot;col-12 col-lg-9 mt-5 mt-lg-0&quot;]/div[@class=&quot;row g-4&quot;]/div[@class=&quot;col-12 col-lg-4&quot;]</value>
+      <webElementGuid>2ede7626-d00d-4e2f-b17a-d16333259c21</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Terjual'])[1]/following::div[3]</value>
+      <webElementGuid>b118ec14-5eaf-4fca-91a4-424ebe0d0395</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Diminati'])[1]/following::div[3]</value>
+      <webElementGuid>54c58aee-ed9a-4493-9be5-06f49be0ec5f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div[2]/div/div</value>
+      <webElementGuid>91ec293d-8b27-4dc4-8708-21b4bd81647c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+              
+  
+      
+
+    
+      Piring cantik
+      Hobi
+      Rp 22.000
+    
+  
+
+            ' or . = '
+              
+  
+      
+
+    
+      Piring cantik
+      Hobi
+      Rp 22.000
+    
+  
+
+            ')]</value>
+      <webElementGuid>fe99fabd-abfe-4d09-bf3c-aa3faabea181</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
