e2700829cbca6220d7b94df062ea7c7d898586b8
zarabiba <fzahrahabibah@gmail.com>
2024-03-01 09:17:59 +0700

    test commit zxahra

---
+++ Project Web/Kelompok_4/Object Repository/SecondHand Website/07 Offer Product/Test Commit zahraaaaa.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Test Commit zahraaaaa</name>
+   <tag></tag>
+   <elementGuidId>8c7eeadb-e7a7-4a68-ae26-d953f6beb2f7</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
