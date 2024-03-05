64697ae8e4f0d829392a894ae9e1c7562d4448e6
wildanbinar <asep.wildan02@gmail.com>
2024-03-05 22:51:42 +0700

    delete test commit

--- Project Web/Kelompok_4/Object Repository/SecondHand Website/Add Product/test commit.rs
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<WebElementEntity>
-   <description></description>
-   <name>test commit</name>
-   <tag></tag>
-   <elementGuidId>15ad48ff-050e-4993-8aab-3d9ddfa8faa1</elementGuidId>
-   <selectorMethod>BASIC</selectorMethod>
-   <useRalativeImagePath>false</useRalativeImagePath>
-</WebElementEntity>
