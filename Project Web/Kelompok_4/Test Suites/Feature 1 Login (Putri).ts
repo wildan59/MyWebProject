1fca1a26f5fc6c8afaa30eb0fab700ba627bffd8
PutriAdriani98 <120355522+PutriAdriani98@users.noreply.github.com>
2024-03-05 21:28:31 +0700

    add test suit fitur login

--- Project Web/Kelompok_4/Test Suites/Feature 1 Login (Putri).ts
+++ Project Web/Kelompok_4/Test Suites/Feature 1 Login (Putri).ts
@@ -12,45 +12,289 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>845ef9e5-712a-445c-a3a6-d6dd12fbc5bc</testSuiteGuid>
    <testCaseLink>
-      <guid>5b5cae80-646d-41e8-a8bf-94de333628e0</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Seller/LGS001 - As a user i should be able to login with credential</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>17376f0d-3a53-4456-8155-ca1d2dd1ca53</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Seller/LGS002 - As a user I shouldnt be able to login using invalid email credential</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>d4b2a538-00f4-4dc4-b7e4-98db3fde26ba</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Seller/LGS003 - As a user I shouldnt be able to login using invalid password</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>e5af0561-64e9-49ec-898e-ca2c456bd621</guid>
+      <guid>4d166bd2-0119-4f4a-bbeb-b22ef50df57f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Buyer/LGB001 - As a user i should be able to login with credential</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>ebb9fe1c-5036-45fb-a1e6-13cff1920c4c</guid>
+      <guid>0dbae525-251f-4111-b374-1fbfbf95bc98</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Buyer/LGB002 - As a user I shouldnt be able to login using invalid email credential</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>82c771c6-fb3c-4b66-ba51-b1b31650ff07</guid>
+      <guid>f7b652ab-9cd9-40c2-a6b0-b2c36815a14f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Buyer/LGB003 - As a user I shouldnt be able to login using invalid password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>803dd2fa-6174-4dc2-a0dc-b939374f2143</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Buyer/LGB004 - As a user I shouldnt be able to login without input email</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6f2ffe86-b445-4925-b5ae-41b6b5431547</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Buyer/LGB005 - As a user I shouldnt be able to login without input password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>edb3a43b-cd2a-4aca-8d74-c496a34012eb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Buyer/LGB006 - As a user I shouldnt be able to login without input email and password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5c43fbeb-3e86-46be-8568-3aae67f9ed03</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Buyer/LGB007 - As a user I shouldnt be able to login using invalid email (not include domain)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5c97c4bd-6389-4911-a398-9aa5be71eac2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Buyer/LGB008 - As a user i should be able to login when offer product</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7e63207f-27a8-4c89-8f2f-838e9a9bdb05</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Seller/LGS008 - As a user i should be able to login when add product</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c8442fb8-9971-43e1-8d0b-acf29512d689</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Seller/LGS007 - As a user I shouldnt be able to login using invalid email (not include domain)</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d1511d7c-e2d4-4a14-9421-130f656c99f1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Seller/LGS006 - As a user I shouldnt be able to login without input email and password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b630432d-57cd-423b-a482-d9a285a3d4c3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Seller/LGS005 - As a user I shouldnt be able to login without input password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d3aa2c7d-1a7c-4ad0-a341-49d6cac12922</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Seller/LGS004 - As a user I shouldnt be able to login without input email</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>705488d5-8b69-4f23-98f6-5a80bdf27cba</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Seller/LGS003 - As a user I shouldnt be able to login using invalid password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e95a416c-d671-469a-99ee-fb1537d6c408</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Seller/LGS002 - As a user I shouldnt be able to login using invalid email credential</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>01507a92-c61f-4ae7-932c-041161df58df</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Featur 1 Login (Putri)/Login As Seller/LGS001 - As a user i should be able to login with credential</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f8ae7bcc-8bfa-451c-8caf-a28317cd40c4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Complete Profile/CP009 - Complete Profile without upload photo when offer product</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ab09227c-01cc-44f5-8720-c4efd6ac1047</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Complete Profile/CP008 - Complete Profile without upload photo when add product</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9795c946-a481-4b5a-87c6-b2c17dc78d4f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Complete Profile/CP007 - Complete Profile from icon product</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>16f60afc-7b84-4eae-bc3b-53da9f2061c2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Complete Profile/CP006 - Complete Profile when add product</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ab057179-889e-48f6-b1b7-257e671d4144</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Complete Profile/CP005 - Complete Profile when offer product</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9585a742-f0f9-47b7-9740-2a82c729b9e0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Complete Profile/CP004 - As a user I shouldnt be able to complete profile without input noHp</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8f4c6c16-df79-42e0-8cab-8b74efe9bd34</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Complete Profile/CP003 - As a user I shouldnt be able to complete profile without input alamat</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6ea0779f-517e-45ec-95af-0a7d942c9d6c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Complete Profile/CP002 - As a user I shouldnt be able to complete profile without select kota</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e684944d-e4fa-469c-8c05-32c5f4f70238</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Complete Profile/CP001 - Complete Profile</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e97de30c-7902-432b-a863-a2cb0722e82e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Register/REG008 - Register when add product without login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b0bb1649-d0f9-4664-bf4f-ded01bb2a598</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7dba02e1-0142-4ead-92dc-ead6d698e579</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Register/REG007 - Register when offer product without login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>eb691935-0993-4945-9953-c33e42744137</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>65f72ef0-0c82-4d63-9d0c-5cb080ffe3ef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Register/REG006 - Register without input username</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>0ba5391a-6ce9-4007-bf8f-ccfb725d0d57</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ff9eb033-117c-4fe0-80db-a777efbd7514</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Register/REG005 - Register without input email and password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>5ca17ad3-e2ed-41c4-bd29-e945fc92ee4b</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0eeb7d5e-e396-45b7-bb9f-24c1a48b9e8a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Register/REG004 - Register without input password</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>27dff713-88a2-46cb-a9c1-576249c32dca</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3a579ca2-e244-4bd0-b208-05f8bac1e410</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Register/REG003 - Register with invalid email</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>f601fa56-b174-482b-bebf-a82812fb6e5e</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7a760b39-531e-485a-bc7c-a957ca217d2b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Register/REG002 - Register with Exist Account</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b32059a7-efd7-413e-90eb-96b8b95b597d</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ac48d560-75e4-4e0c-9fdf-d1ac753fdbe2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Register/REG001 - Register with New Account</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>da06c5af-e367-434c-9dab-a2d6d815ca52</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cce57e53-6091-4c87-ada3-ad50a432e40b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Logout/LGT001 - Logout</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
