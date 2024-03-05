da9d618a56bf2014a2767099ee347baf9b544130
zarabiba <fzahrahabibah@gmail.com>
2024-03-06 02:50:53 +0700

    IDWEB001_Zahra compiling

--- Project Web/Kelompok_4/Test Suites/Test Suites/Feature 4 (1) - Accept Offer.ts
+++ Project Web/Kelompok_4/Test Suites/Test Suites/Feature 4 (1) - Accept Offer.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Feature 4 - Accept Offer</name>
+   <name>Feature 4 (1) - Accept Offer</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
